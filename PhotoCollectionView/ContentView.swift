a0c90d549567380a0a3ca1593b7a74f6628d15ac
Frank S. Bara <fbara1@yahoo.com>
2019-11-03 00:23:10 -0500

    Initial Commit

---
+++ PhotoCollectionView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoCollectionView
+//
+//  Created by Frank Bara on 11/3/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
